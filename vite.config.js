eeb860ccd219101c3bd9bd5764a433929cdc5801
ayesh156 <sdachathuranga@gmail.com>
2025-01-05 23:31:08 +0530

    Set Router Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -10,10 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  css: {
-    postcss: './postcss.config.cjs', // Specify the PostCSS config file explicitly
-  },
-});
+})
